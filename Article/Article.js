3b0647016cea8fe4eff32ae06f6ed4de72c1fc3a
Ikechukwu Eze <iykekings36@gmail.com>
2019-05-16 00:00:53 +0100

    Stretch: Custom delete & expand button with animations

--- Article/Article.js
+++ Article/Article.js
@@ -7,15 +7,31 @@ class Article {
     // create a reference to the ".expandButton" class.
     this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    //this.expandButton.textContent = 'expand';
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+    // Extras
+    this.arrowExpandBtn = domElement.querySelector('.btn.expand');
+    this.arrowExpandBtn.addEventListener(
+      'click',
+      this.expandArticle.bind(this)
+    );
+    // delete button
+    this.deleteBtn = domElement.querySelector('.btn.remove');
+    this.deleteBtn.addEventListener('click', this.deleteArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+    const arrowStr = this.arrowExpandBtn.textContent;
+    this.arrowExpandBtn.textContent = arrowStr === '∧' ? '∨' : '∧';
+  }
+  deleteArticle() {
+    this.domElement.style.opacity = '0.3';
+    this.domElement.style.height = '25px';
+    setTimeout(() => this.domElement.remove(), 1000);
   }
 }
 
